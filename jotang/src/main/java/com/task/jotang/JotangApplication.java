e7ebbf87673abc3ff14fc3dfd6374440505631e1
yifei-creat <143923457+yifei-creat@users.noreply.github.com>
2023-10-12 19:14:11 +0800

    Add files via upload

---
+++ jotang/src/main/java/com/task/jotang/JotangApplication.java
@@ -0,0 +1,13 @@
+package com.task.jotang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JotangApplication.class, args);
+    }
+
+}
---
+++ jotang/src/test/java/com/task/jotang/JotangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.jotang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
